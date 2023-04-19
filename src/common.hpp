516bc2066e4f7efa91573a41217d6240abd8add4
Yimura <24669514+Yimura@users.noreply.github.com>
2023-04-20 00:01:51 +0200

    feat(common): common includes

--- src/common.hpp
+++ src/common.hpp
@@ -3,9 +3,18 @@
 #include <Windows.h>
 
 #include <atomic>
+#include <chrono>
+#include <thread>
+
+#include <array>
+
+#include <cstddef>
+#include <string_view>
 
 namespace NewBase
 {
+    using namespace std::chrono_literals;
+
     extern std::atomic<bool> g_Running;
     extern HINSTANCE g_DllInstance;
     extern HANDLE g_MainThread;
