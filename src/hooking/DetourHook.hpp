ed14c9e680dc7a066c010b497ba7bcab7689ed28
Yimura <24669514+Yimura@users.noreply.github.com>
2023-04-20 00:03:50 +0200

    feat: add files

---
+++ src/hooking/DetourHook.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <string_view>
+
+namespace NewBase
+{
+    class DetourHook
+    {
+    private:
+        /* data */
+    public:
+        DetourHook(/* args */);
+        ~DetourHook() = default;
+    };
+}
---
+++ src/hooking/Hooking.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace NewBase
+{
+    class Hooking
+    {
+    private:
+        
+        
+    public:
+        Hooking(/* args */);
+        ~Hooking();
+
+    };
+}
