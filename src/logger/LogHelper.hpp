d1611c78dbb8ffe99ea43bc3aa0d287f5a650e26
Yimura <24669514+Yimura@users.noreply.github.com>
2023-05-06 23:52:52 +0200

    fix(LogHelper): fix HEX macro

--- src/logger/LogHelper.hpp
+++ src/logger/LogHelper.hpp
@@ -7,7 +7,7 @@ namespace NewBase
 {
 #define ADD_COLOR_TO_STREAM(color) "\x1b[" << int(color) << "m"
 #define RESET_STREAM_COLOR "\x1b[0m"
-#define HEX(addr) "0x" << std::hex << std::uppercase << (void*)addr << std::dec << std::nouppercase
+#define HEX(value) "0x" << std::hex << std::uppercase << DWORD64(value) << std::dec << std::nouppercase
 
     class LogHelper final
     {
