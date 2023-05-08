f01f3a1276d836c5caa8ceb431b34c0ee0661ab3
Yimura <24669514+Yimura@users.noreply.github.com>
2023-05-08 21:53:03 +0200

    chore(SwapChain): stupid wrong indents

--- src/hooks/GUI/SwapChain.cpp
+++ src/hooks/GUI/SwapChain.cpp
@@ -1,17 +1,17 @@
-#include "hooks/Hooks.hpp"
 #include "hooking/VMTHook.hpp"
+#include "hooks/Hooks.hpp"
 
 namespace NewBase
 {
-    HRESULT SwapChain::Present(IDXGISwapChain* that, UINT syncInterval, UINT flags)
-    {
+	HRESULT SwapChain::Present(IDXGISwapChain* that, UINT syncInterval, UINT flags)
+	{
 		return BaseHook::Get<SwapChain::Present, VMTHook<SwapChain::VMTSize>>()->Original<decltype(&Present)>(SwapChain::VMTPresentIdx)(that, syncInterval, flags);
-    }
+	}
 
-    HRESULT SwapChain::ResizeBuffers(IDXGISwapChain* that, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags)
-    {
-        LOG(VERBOSE) << "Buffers have been resized";
+	HRESULT SwapChain::ResizeBuffers(IDXGISwapChain* that, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags)
+	{
+		LOG(VERBOSE) << "Buffers have been resized";
 
-        return BaseHook::Get<SwapChain::Present, VMTHook<SwapChain::VMTSize>>()->Original<decltype(&ResizeBuffers)>(SwapChain::VMTResizeBuffersIdx)(that, bufferCount, width, height, newFormat, swapChainFlags);
-    }
+		return BaseHook::Get<SwapChain::Present, VMTHook<SwapChain::VMTSize>>()->Original<decltype(&ResizeBuffers)>(SwapChain::VMTResizeBuffersIdx)(that, bufferCount, width, height, newFormat, swapChainFlags);
+	}
 }
