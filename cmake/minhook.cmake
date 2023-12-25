4f77055f7e1bb3ef61ee178fa3bcb2d7cc0e7cd5
Yimura <24669514+Yimura@users.noreply.github.com>
2023-12-25 20:27:15 +0100

    chore(cmake): update minhook

--- cmake/minhook.cmake
+++ cmake/minhook.cmake
@@ -6,7 +6,7 @@ message(STATUS "Setting up ${LIB_NAME}")
 FetchContent_Declare(
     ${LIB_NAME}
     GIT_REPOSITORY https://github.com/TsudaKageyu/minhook.git
-    GIT_TAG        98b74f1fc12d00313d91f10450e5b3e0036175e3
+    GIT_TAG        f5485b8454544c2f034c78f8f127c1d03dea3636
 	GIT_PROGRESS TRUE
 )
 FetchContent_MakeAvailable(${LIB_NAME})
